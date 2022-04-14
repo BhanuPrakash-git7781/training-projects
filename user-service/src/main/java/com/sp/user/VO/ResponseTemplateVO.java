1ccda3ce8e88daa5c17d14af949fe47021122cb2
sachinpatil101 <sachinbpatil18@gmail.com>
2022-04-14 12:45:53 +0530

    added user-dept microservices

---
+++ user-service/src/main/java/com/sp/user/VO/ResponseTemplateVO.java
@@ -0,0 +1,16 @@
+package com.sp.user.VO;
+
+import com.sp.user.entity.User;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ResponseTemplateVO {
+
+    private User user;
+    private Department department;
+}
